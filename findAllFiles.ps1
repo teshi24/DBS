fcfcdae39d5ffa5761aceb3699e75bd0c466b894
teshi24 <nadja.stadi@gmail.com>
2023-03-21 15:54:57 +0100

    first version findAllFiles and getMetadataOfOneFile

---
+++ findAllFiles.ps1
@@ -0,0 +1,23 @@
+﻿[string[]]$excludeFolders = @(
+#".idea*"
+"Android"
+)
+
+echo $excludeFolders
+
+ dir -Path D:\source* -Filter *.* -Recurse -ErrorAction SilentlyContinue -Exclude $excludeFolders | %{
+    $allowed = $true
+    foreach ($exclude in $excludeFolders) {
+        if (($_.FullName).Contains($exclude)) { 
+            $allowed = $false
+            break
+        }
+    }
+    if ($allowed) {
+        $_.FullName
+    }
+ }
+
+#Get-ChildItem -Path D:\source* -Filter *.* -Recurse -ErrorAction SilentlyContinue -Force
+#Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
+#Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
---
+++ getMetadataOfOneFile.ps1
@@ -0,0 +1,41 @@
+﻿Function List-FileMetaData  
+{  
+    param([Parameter(Mandatory=$True)][string]$File = $(throw "Parameter -File is required."))  
+  
+    if(!(Test-Path -Path $File))  
+    {  
+        throw "File does not exist: $File"  
+        Exit 1  
+    }  
+  
+    $tmp = Get-ChildItem $File  
+    $pathname = $tmp.DirectoryName  
+    $filename = $tmp.Name  
+  
+    [HashTable]$hash = @{} 
+    try{ 
+        $shellobj = New-Object -ComObject Shell.Application  
+        $folderobj = $shellobj.namespace($pathname)  
+        $fileobj = $folderobj.parsename($filename)  
+         
+        for($i=0; $i -le 294; $i++)  
+        {  
+            $name = $folderobj.getDetailsOf($null, $i); 
+            if($name){ 
+                $value = $folderobj.getDetailsOf($fileobj, $i); 
+                $hash[$($name)] = $($value) 
+                #$hash[$($name)] = '' 
+            } 
+        }  
+    }finally{ 
+        if($shellobj){ 
+            [System.Runtime.InteropServices.Marshal]::ReleaseComObject([System.__ComObject]$shellobj) | out-null 
+        } 
+    } 
+ 
+    #return New-Object PSObject -Property $hash 
+    return $hash 
+}  
+ 
+#example usage  
+List-FileMetaData -File "D:\FehlerCode_OneDrive.PNG"
