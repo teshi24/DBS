b8b6031ddb0e53f3d9d4a4c31ccb702cdb869e21
teshi24 <nadja.stadi@gmail.com>
2023-03-25 12:46:05 +0100

    Iteration working, as well as List-FileMetaData and List-DirMetaData

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # DBS
+
 Database Systems Projekt HSLU
+
+## Usage Data Load Scripts
+
+**! still under development, not yet fully usable !**
+
+### Installation
+
+1. Download Windows PowerShell as described in the modul description
+    [TODO]: (add more detailed description / link)
+2. Open Windows PowerShell ISE as an admin
+3. Open scripts in this repository
+4. Run the script getMetadataOfOneFile.ps1 (todo: adapt in the end)
+5. If you get this error:
+    + CategoryInfo          : Sicherheitsfehler: (:) [], ParentContainsErrorRecordException
+    + FullyQualifiedErrorId : UnauthorizedAccess
+    You need to enable the script runs on your laptop temporarly
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+    don't forget to reset this after the script execution!!
+    Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
+6. todo fix this description
+
--- findAllFiles.ps1
+++ findAllFiles.ps1
@@ -1,23 +1,132 @@
 ﻿[string[]]$excludeFolders = @(
-#".idea*"
-"Android"
+".gradle",
+".idea",
+".vscode",
+".git",
+"cache",
+"build",
+"target",
+"temp",
+"lib",
+"node_modules"
 )
 
+
+echo "The directories with the following names will be ignored"
 echo $excludeFolders
+echo "=================="
 
- dir -Path D:\source* -Filter *.* -Recurse -ErrorAction SilentlyContinue -Exclude $excludeFolders | %{
-    $allowed = $true
-    foreach ($exclude in $excludeFolders) {
-        if (($_.FullName).Contains($exclude)) { 
-            $allowed = $false
-            break
+
+Function List-FileMetaData  
+{  
+    param([Parameter(Mandatory=$True)][string]$File = $(throw "Parameter -File is required."))  
+  
+    if(!(Test-Path -Path $File))  
+    {  
+        throw "File does not exist: $File"  
+        Exit 1  
+    }  
+  
+    $tmp = Get-ChildItem $File  
+    $pathname = $tmp.DirectoryName  
+    $filename = $tmp.Name  
+  
+    [HashTable]$hash = @{} 
+    try{ 
+        $shellobj = New-Object -ComObject Shell.Application  
+        $folderobj = $shellobj.namespace($pathname)  
+        $fileobj = $folderobj.parsename($filename)  
+         
+        for($i=0; $i -le 294; $i++)  
+        {  
+            $name = $folderobj.getDetailsOf($null, $i); 
+            if($name){ 
+                $value = $folderobj.getDetailsOf($fileobj, $i); 
+                $hash[$($name)] = $($value) 
+                #$hash[$($name)] = '' 
+            } 
+        }  
+    }finally{ 
+        if($shellobj){ 
+            [System.Runtime.InteropServices.Marshal]::ReleaseComObject([System.__ComObject]$shellobj) | out-null 
+        } 
+    } 
+ 
+    #return New-Object PSObject -Property $hash 
+    return $hash 
+}  
+
+
+Function List-DirMetaData  
+{  
+    param([Parameter(Mandatory=$True)][string]$Dir = $(throw "Parameter -Dir is required."))  
+  
+    if(!(Test-Path -Path $Dir))  
+    {  
+        throw "Dir does not exist: $Dir"  
+        Exit 1  
+    }
+  
+    $tmp = Get-ChildItem $Dir  
+    $pathname = $tmp.DirectoryName
+  
+    [HashTable]$hash = @{} 
+    try{ 
+        $fso = New-Object -ComObject Shell.Application
+        $folderobj = $fso.namespace($Dir)
+
+        $folderSelf = $folderobj.self()
+        
+        $hash['name'] = $folderSelf.name()
+        $hash['modifyDate'] = $folderSelf.modifyDate()
+        $hash['path'] = $folderSelf.path()
+        $hash['size'] = $folderSelf.size()
+        $hash['type'] = $folderSelf.type()
+
+    }finally{ 
+        if($shellobj){ 
+            [System.Runtime.InteropServices.Marshal]::ReleaseComObject([System.__ComObject]$shellobj) | out-null 
+        } 
+    } 
+ 
+    #return New-Object PSObject -Property $hash 
+    return $hash 
+}  
+
+
+
+function handlePrintingOfAllData($path) {
+    echo "handle ${path}"
+    echo " "
+    echo $_.FullName
+    echo " "
+    if (Test-Path -Path $_.FullName -PathType Leaf) {
+        $list = List-FileMetaData -File $path
+        ConvertTo-Csv -InputObject $list
+        $path >> D:\DBS_output_files.txt
+    } else {
+        $list = List-DirMetaData -Dir $path
+        ConvertTo-Csv -InputObject $list
+        $path >> D:\DBS_output_directories.txt
+    }
+}
+
+dir -Path D:\source -Filter *.* -ErrorAction SilentlyContinue -Exclude $excludeFolders | %{
+   handlePrintingOfAllData($_.FullName)
+}
+
+
+function recursive($path) {
+    dir -Path $path -Filter *.* -ErrorAction SilentlyContinue -Exclude $excludeFolders | %{
+        handlePrintingOfAllData($_.FullName)
+        if (Test-Path -Path $_.FullName -PathType Leaf) {
+            return
         }
+        recursive($_.FullName)
     }
-    if ($allowed) {
-        $_.FullName
-    }
- }
+}
 
-#Get-ChildItem -Path D:\source* -Filter *.* -Recurse -ErrorAction SilentlyContinue -Force
+#recursive D:\source*
+
 #Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
 #Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
